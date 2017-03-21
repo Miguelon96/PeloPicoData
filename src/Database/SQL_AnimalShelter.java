898a2150b895be38d5453747b80bbabe39b8dc0f
Miguel Angel <Miguel Angel@MiguelAngel-PC>
2017-03-21 16:24:44 +0100

    Clases de SQL creadas

---
+++ src/Database/SQL_AnimalShelter.java
@@ -0,0 +1,26 @@
+package Database;
+
+public class SQL_AnimalShelter {
+
+public void connect(){
+	
+}
+
+
+public void disconnect(){
+	
+}
+
+public void insertx(){
+	
+}
+
+public void getAllx(){
+	
+}
+
+public void createTable(){
+	
+}
+
+}
