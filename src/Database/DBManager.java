d0372fbca0e414f1781b309c8f77cd59a89a0932
melvinmen <jm.fernadez30@usp.ceu.es>
2017-03-23 15:11:19 +0100

    connect

---
+++ src/Database/DBManager.java
@@ -0,0 +1,27 @@
+package Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBManager {
+	
+public static void connect(String args[]){
+	try {
+		// Open database connection
+		Class.forName("org.sqlite.JDBC");
+		Connection c = DriverManager.getConnection("jdbc:sqlite:./db/company.db");
+		c.createStatement().execute("PRAGMA foreign_keys=ON");
+		System.out.println("Database connection opened.");
+		
+		// Here is where I do things with the database
+		
+		// Close database connection
+		c.close();
+		System.out.println("Database connection closed.");
+	} catch (Exception e) {
+		e.printStackTrace();
+	}
+	
+}
+
+}
